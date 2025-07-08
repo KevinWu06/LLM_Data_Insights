4d35174085786de7caa65b7b10d8c81027eabe90
KevinWu06 <yw4448@columbia.edu>
2025-07-08 11:04:03 -0500

    Re-made banner image map after verifying links

--- frontend/src/embeddedImages.js
+++ frontend/src/embeddedImages.js
@@ -1,37 +1,49 @@
 const bannerImageMap = {
-    "Improved PFS":                 "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-CCA-TIB-001475-1/300x600/",
-    "Disease Control Rate":         "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-CCA-TIB-001475-2/160x600/",
-    "Tibsovo + AZA":                "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-AML-TIB-002608-2/300x600/",
-    "#1 Prescribed" :               "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-AML-TIB-002604-2/300x600/",
-    "Improved mOS" :                "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-AML-TIB-002522-1/300x600/",
-    "Go Deeper" :                   "https://banners.culthealth.com/SERVIER/TIBSOVO/MDS/SVR-MDS-TIB-002671-2/300x600/300x600.png",
-    "Newly Diagnosed CR" :          "https://banners.culthealth.com/SERVIER/TIBSOVO/AML/SVR-AML-TIB-002522-2/HTML/300x600/",
-    "Now Approved (Anim.)" :        "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003067-1/HTML/300x600/", //duplicate 2
-    "Now Approved (Stat.)" :        "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003067-1/HTML/300x600/", //duplicate 2
-    "First-in-class" :              "https://banners.culthealth.com/SERVIER/CCA/SVR-CCA-TIB-002939-1/300x600/",
-    "NCCN" :                        "https://banners.culthealth.com/SERVIER/CCA/SVR-CCA-7902428903/300x600/",
-    "MDS Now Approved" :            "https://banners.culthealth.com/SERVIER/MDS/SVR-MDS-TIB-002698-2/300x600/", 
-    "MDS New Treatment" :           "https://banners.culthealth.com/SERVIER/MDS/SVR-MDS-TIB-002698-1/300x600/",
-    "Laser Video" :                 "https://banners.culthealth.com/SERVIER/AML/SVR-AML-TIB-002843-1/300x600/",
-    "FDA Approved" :                "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003038-1/HTML/300x600/",
-    "Rob Testimonial (Anim.)" :     "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-004020-1/v18/a/300x600/",
-    "Glioma DSA" :                  "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-002900-1/HTML/300x600/", //not sure
-    "ASH Branded" :                 "",
-    "CR and DOCR" :                 "",
-    "KOL" :                         "",
-    "Legacy Banner" :               "",
-    "mlDH1 BioPharm" :              "https://banners.culthealth.com/SERVIER/AML/AML-TIB-003717-1/300x600/", //not sure
-    "Promotional HCP (Anim.)" :     "https://banners.culthealth.com/SERVIER/GLM/SVR-VOR-8889273085/1b/300x600/", //not sure
-    "Promotional HCP (Stat.)" :     "https://banners.culthealth.com/SERVIER/GLM/SVR-VOR-8889273085/1b/300x600/", //not sure, and static is a file
-    "R1" :                          "",
-    "Rob Testimonial (Stat.)" :     "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-004020-1/v18/a/300x600/", //static is a file
-    "Testing" :                     "",
-    "Testing (V1)" :                "",
-    "Testing (V2)" :                "",
-    "Transfus. Indep." :            "",
-    "Unbranded" :                   "",
-    "US-00818" :                    "",
-    "US-02033" :                    "",
+    "Improved PFS":                 "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-CCA-TIB-001475-1/300x600/", //verified
+    "Disease Control Rate":         "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-CCA-TIB-001475-2/160x600/", //verified
+    "Tibsovo + AZA":                "https://banners.culthealth.com/SERVIER/TIBSOVO/AML/SVR-AML-TIB-002604-1/HTML/300x600/", //verified
+    "#1 Prescribed" :               "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-AML-TIB-002604-2/300x600/", //verified
+    "Improved mOS" :                "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-AML-TIB-002522-1/300x600/", //verified
+    "Go Deeper" :                   "https://banners.culthealth.com/SERVIER/MDS/SVR-MDS-TIB-002671-1/300x600/", //verified
+    "Newly Diagnosed CR" :          "https://banners.culthealth.com/SERVIER/TIBSOVO/AML/SVR-AML-TIB-002522-2/HTML/300x600/", //verified
+    "Now Approved (Anim.)" :        "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003067-1/HTML/300x600/", //verified
+    "Now Approved (Stat.)" :        "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003038-1/HTML/300x600/", //verified
+    "NCCN" :                        "https://banners.culthealth.com/SERVIER/AML/SVR-AML-TIB-002854-1/300x600/", //verified
+    "MDS Now Approved" :            "https://banners.culthealth.com/SERVIER/MDS/SVR-MDS-TIB-002698-2/300x600/", //verified
+    "MDS New Treatment" :           "https://banners.culthealth.com/SERVIER/MDS/SVR-MDS-TIB-002698-1/300x600/", //verified
+    "Rob Testimonial (Anim.)" :     "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-004020-1/v18/a/300x600/", //verified
+    "Glioma DSA" :                  "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-002900-1/HTML/300x600/", //verified
+    "ASH Branded" :                 "https://banners.culthealth.com/SERVIER/AML/AML-TIB-003469-3/300x600/", //verified
+    "KOL" :                         "https://banners.culthealth.com/SERVIER/TIBSOVO/SVR-AML-TIB-002608-2/300x600/", //verified
+    "Promotional HCP (Anim.)" :     "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003602-1/v28/", //verified
+    "Promotional HCP (Stat.)" :     "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003602-2/v6/300x50/", //verified
+    "R1" :                          "https://banners.culthealth.com/SERVIER/AML/AML-TIB-003717-1/300x600/", //verified
+    "Testing" :                     "https://banners.culthealth.com/SERVIER/CCA/SVR-CCA-TIB-002939-1/300x600/", //verified
+    "Testing (V1)" :                "https://banners.culthealth.com/SERVIER/AML/AML-TIB-003938-1/v20/300x600/", //verified
+
+    "US-00818" :                    "", //missing
+
+    "First-in-class" :              "", // "https://banners.culthealth.com/SERVIER/CCA/SVR-CCA-TIB-002939-1/300x600/", //missing
+
+    "Laser Video" :                 "", //"https://banners.culthealth.com/SERVIER/AML/SVR-AML-TIB-002843-1/300x600/" //missing
+
+    "FDA Approved" :                "", //"https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-003038-1/HTML/300x600/", //missing
+
+    "CR and DOCR" :                 "", //missing
+
+    "Legacy Banner" :               "", //missing
+
+    "mlDH1 BioPharm" :              "", // "https://banners.culthealth.com/SERVIER/AML/AML-TIB-003717-1/300x600/", //missing
+
+    "Rob Testimonial (Stat.)" :     "", // "https://banners.culthealth.com/SERVIER/GLM/GLM-VOR-004020-1/v18/a/300x600/", //static is a file, missing
+
+    "US-02033" :                    "", //missing
+
+    "Testing (V2)" :                "", //missing
+
+    "Transfus. Indep." :            "", //missing
+
+    "Unbranded" :                   "", //missing       
 
 };  
 
